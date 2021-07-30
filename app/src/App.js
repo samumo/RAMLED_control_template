8d87196ec41926b512728f7ba2ed72ca2f9d66fd
Samuel Morgan <74990386+samumo@users.noreply.github.com>
2021-07-30 14:11:35 +0100

    api_working

--- app/src/App.js
+++ app/src/App.js
@@ -5,7 +5,7 @@ import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container'
 import './App.css';
-
+import $ from 'jquery';
 
 
 
@@ -20,37 +20,47 @@ class App extends React.Component {
     this.test = this.test.bind(this);
   }
   
-  test(e) {
-    // - POST
-    e.preventDefault();
+  // test(e) {
+  //   e.preventDefault();
     
-    // creates entity
+  //   let uri = "http://192.168.8.104:8080/api/v1.0/set_name_mask";
+  //   let res = encodeURI(uri);
+  //   console.log(JSON.stringify({"name_mask":["A1"],"fade": 0.3,"timeout":2}));
+  //   fetch(res, {
+  //     "method": "POST",
+  //     "headers": {
+  //       // "content-type": 'application/json',
+  //       // "accept": "text/plain",
+  //       // "Accept": "/",
+  //       // 'mode': 'cors'
+  //     },
+  //     "body": JSON.stringify({"name_mask":["A1"],"fade": 0.3,"timeout":2})
+  //   })
+  //   .then(response => response.json())
+  //   .then(response => {
+  //     this.setState({
+  //       ApiResponse: response
+  //     });
+  //     console.log(response)
+  //   })
+  //   .catch(err => {
+  //     // this.setState({
+  //     //   ApiResponse: err
+  //     // })
+  //     console.log(err);
+  //   });
+  // }
+
+  test(e) {
+    const jsonData = JSON.stringify({"name_mask": ["A1", "A2"], "fade": 0.1, "auto_off": 3});
     let uri = "http://192.168.8.104:8080/api/v1.0/set_name_mask";
     let res = encodeURI(uri);
-    console.log(JSON.stringify({"name_mask":["A1"],"fade": 0.3,"timeout":2}));
-    fetch(res, {
-      "method": "POST",
-      "headers": {
-        // "content-type": 'application/json',
-        // "accept": "text/plain",
-        // "Accept": "/",
-        // 'mode': 'no-cors'
-      },
-      "body": JSON.stringify({"name_mask":["A1"],"fade": 0.3,"timeout":2})
-    })
-    .then(response => response.json())
-    .then(response => {
-      this.setState({
-        ApiResponse: response
-      });
-      console.log(response)
-    })
-    .catch(err => {
-      // this.setState({
-      //   ApiResponse: err
-      // })
-      console.log(err);
-    });
+    e.preventDefault();
+    $.ajax(res, {
+      data:jsonData,
+      method: "POST",
+      contentType: "application/json"
+   });
   }
 
   render() {
@@ -65,7 +75,6 @@ class App extends React.Component {
           <h2>{this.state.ApiResponse}</h2>
         </Jumbotron>
       </Container>
-
     )
   }
 }
